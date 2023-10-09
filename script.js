3f452e3fad3d7b109cae54324aa95006c872dbbc
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-10-09 11:15:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById("swap").addEventListener("click",()=>{
 		document.getElementById("app").classList.toggle("night");
+		document.getElementById("swap").classList.toggle("button_night");
 });
 
